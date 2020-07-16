f8a994b661540675150782a0806aa552cf66bd0d
Aamir Farooq <aamir@farooq.xyz>
2020-07-16 02:21:37 +0200

    Push update when found

--- cogs/watcher.py
+++ cogs/watcher.py
@@ -1,8 +1,8 @@
 # import asyncio
 import discord
 import feedparser
+import sqlite3
 from discord.ext import commands, tasks
-
 
 class MembersCog(commands.Cog):
     def __init__(self, bot):
@@ -23,15 +23,16 @@ class MembersCog(commands.Cog):
         # track previous post names -- so we don't repost same one again
         prev_posts = [something["title"] for something in self.data_old.entries]
         # get new posts
+        # new_posts = [post for post in data.entries if checks(post, prev_posts, max_prev_date) ]
         new_posts = [post for post in data.entries if checks(post, prev_posts, max_prev_date) ]
         # if there rae new posts
         if (len(new_posts) > 0):
             # check thier tags
             for post in new_posts:
                 print(f'NEW GOOD ENTRY: {post.title} {post.link}')
-                await post_update(post, self.bot)
+                await check_new_entries(post, self.bot)
 
-        data_old = data
+        self.data_old = data
         
     @watcher.before_loop
     async def before_printer(self):
@@ -40,10 +41,55 @@ class MembersCog(commands.Cog):
 def checks(post, prev_posts, max_prev_date):
     filters = ["iOS", "watchOS", "macOS", "iPadOS", "tvOS"]
     device = post["title"].split(" ")[0]
-    return post["published_parsed"] > max_prev_date and post["title"] not in prev_posts and device in filters
+    # TEST CODE
+    # return post["published_parsed"] > max_prev_date and post["title"] not in prev_posts and device in filters
+    return device in filters
 
-def post_update(post, bot):
-    pass
+async def check_new_entries(post, bot):
+    device = post["title"].split(" ")[0]
+    try:
+        conn = sqlite3.connect('db.sqlite')
+        c = conn.cursor()
+        c.execute("SELECT * FROM configs")
+        res = c.fetchall()
+    finally:
+        conn.close()
+    
+    for guild in res:
+        # TEST CODE
+        # if guild[6] == -1:
+        #     continue
+        
+        index = {
+            "iOS": 1,
+            "macOS": 2,
+            "iPadOS": 3,
+            "watchOS": 4,
+            "tvOS": 5,
+        }[device]
+        
+        role_id = guild[index]
+        # TEST CODE
+        role_id = 525250808447631370
+
+        if role_id != -1:
+            await push_update(bot, guild, device, role_id, post)
+
+async def push_update(bot, guild_info, device, role_id, post):
+    guild = bot.get_guild(guild_info[0])
+
+    if guild is None:
+        return
+
+    guild_channels = guild.channels
+    # TEST CODE
+    # channel = discord.utils.get(guild_channels, id=guild_info[6])
+    channel = discord.utils.get(guild_channels, id=621704381053534257)
+
+    if channel is None:
+        return
+    
+    await channel.send(f'<@&{role_id}> New release! {post["title"]}\n{post["link"]}')
 
 def setup(bot):
     bot.add_cog(MembersCog(bot))
